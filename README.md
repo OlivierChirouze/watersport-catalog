decff24d0c33396fc4fc2b3cde68c7980bd6b061
Olivier Chirouze <o.chirouze@criteo.com>
2021-07-27 22:21:07 +0200

    Update readme and JSON model

--- README.md
+++ README.md
@@ -4,3 +4,22 @@ Small library to scrape information about water sports material:
 - windsurfing sails
 - windsurfing boards
 - ... more to come
+
+## Content
+The library contains both the code to scrape information (in `src`)
+and the latest catalog information in JSON format (in `products`).
+
+The **product definition** is described in details in [model.ts](./src/model.ts)
+
+## Currently supported brands
+
+- [Gaastra](https://ga-windsurfing.com/)
+- [Exocet](https://www.exocet-original.fr/)
+- [Patrik](https://patrik-windsurf.com/)
+
+## Todo
+- [ ] scrape brand info (logo, description, links)
+- [ ] support all models from the current brands
+- [ ] add brands
+- [ ] add kitesurfing
+- [ ] add wingfoiling
--- src/model.ts
+++ src/model.ts
@@ -1,13 +1,4 @@
-export enum Program {
-  beginner = "beginner",
-  kids = "kids",
-  wave = "wave",
-  freeride = "freeride",
-  freestyle = "freestyle",
-  slalom = "slalom",
-  race = "race"
-}
-
+// Water sport activity
 export enum Activity {
   windsurf = 'windsurf',
   kitesurf = 'kitesurf',
@@ -18,66 +9,132 @@ export enum Activity {
   wingsurf = 'wingsurf'
 }
 
+// "type" of activity
+export enum Program {
+  beginner = "beginner",
+  kids = "kids",
+  wave = "wave",
+  freeride = "freeride",
+  freestyle = "freestyle",
+  slalom = "slalom",
+  race = "race"
+}
+
+// Big categories of gear, regardless of the activity
+// example: a "windsurf board" (GearType) can in fact be used for windfoiling (Activity)
 export enum GearType {
   windsurfBoard = 'windsurfBoard',
-  surfBoard = 'surfBoard',
-  sail = "sail"
+  sail = "sail",
+  // surfBoard = 'surfBoard',
+  // kite = "kite"
+  // ... to be continued!
 }
 
-export interface Picture<T> {
-  variant: Partial<T>;
-  url: string;
-}
-
-export interface GearModel<T> {
+// This is the actual product a brand is selling
+// The VariantType should be a simple type with a few properties. It's the type that makes
+// all "variants" of this gear, unique
+// Example:
+// interface MyVariant {
+//   size: number;
+//   construction: string;
+// }
+export interface GearModel<VariantType> {
+  type: GearType;
   brandName: string;
+  // A name should be unique for a brand and a year range
   name: string;
   // A model can last for a few years
   years: number[];
   infoUrl?: string;
-  type: GearType;
+  // Gears can apply to multiple activities (ex: windsurfing and windfoiling)
   activities: Activity[];
+  // Main programs the gear is targeting
   programs: Program[];
-  dimensions: (keyof T)[];
-  variants: GearVariant<T>[];
   // Description is per language
   description: {[language: string]: string};
-  pictures: Picture<T>[];
+
+  // List of pictures that are more or less specific to a variant
+  pictures: Picture<VariantType>[];
+
+  // List of keys that define "variants" of this gear.
+  // Example: ['size', 'construction'] means you expect variants to have different size and construction values
+  dimensions: (keyof VariantType)[];
+
+  // The actual variants of this gear
+  // Each variant has a "variant" property, of type VariantType, that defines how this variant is "unique"
+  variants: GearVariant<VariantType>[];
 }
 
-// Examples of names:
-// Gaastra, Vapor Air, R, 5.6, 2020
-// Gaastra, Vapor Air, SL, 5.6, 2020
-// Gaastra, Vapor, 5.2, 2021
-// Gaastra, Manic, 3.7, C2, 2019
-// Gaastra, Manic, 4.0, C1, 2019
-// Gaastra, Hybrid, 6.4, C3, 2020
-// Patrik, qt-wave, GBM, 83, 2019
+// A picture applies to a particular variant of the gear
+// (or to any variant, if variant is empty)
+export interface Picture<VariantType> {
+  variant: Partial<VariantType>;
+  url: string;
+}
 
-export interface GearVariant<V> {
-  variant: Partial<V>;
+// A gear variant, that is defined at least by it's variant "dimensions"
+export interface GearVariant<VariantType> {
+  variant: Partial<VariantType>;
 }
 
 // ---------------------------------------------- Windsurfing / windfoiling sails
+
+// A windsurfing sail
+export interface WindsurfSail<VariantType> extends GearVariant<VariantType> {
+  // The main info (the only one mandatory!): what is the surface of this sail
+  surfaceM2: number;
+  // Is it a fixed or vario top?
+  topType?: WindsurfSailTopType;
+  // Luff length
+  luffLengthCm?: number;
+  // Boom length
+  boomLengthCm?: number;
+  // The list of mast lengths that would be compatible with this sail
+  mastLengthsCm?: number[];
+  // The list of mast extension lengths that would be compatible with this sail
+  mastExtensionLengthsCm?: number[];
+  // The list of mast IMCS (Index Mast Check System) that would be compatible with this sail
+  mastIMCS?: number[];
+  // Number of battens
+  battenCount?: number;
+  // Number of cams
+  camCount?: number;
+  // Weight
+  weightKg?: number;
+}
+
 export enum WindsurfSailTopType {
   fixed = "fixed",
   vario = "vario"
 }
 
-export interface WindsurfSail<V> extends GearVariant<V> {
-  surfaceM2: number;
-  topType?: WindsurfSailTopType;
-  luffLengthCm?: number;
-  boomLengthCm?: number;
-  mastLengthsCm?: number[];
-  mastExtensionLengthsCm?: number[];
-  mastIMCS?: number[];
-  battenCount?: number;
-  camCount?: number;
+// ---------------------------------------------- Windsurfing / windfoiling boards
+
+// A windsurfing board
+export interface WindsurfBoard<VariantType> extends GearVariant<VariantType> {
+  // The main info (the only one mandatory!): what is the volume of this board
+  volumeL: number;
+  // Max length
+  lengthCm?: number;
+  // Max width
+  widthCm?: number;
+  // Weight
   weightKg?: number;
+  // Number of strap inserts
+  strapInsertCount?: number;
+  // Configuration of fins: how many of each type
+  fins?: FinConfig[];
+  // Compatible sails: min and max surface
+  sailRange?: SailRange;
 }
 
-// ---------------------------------------------- Windsurfing / windfoiling boards
+// Number and box types of fin boxes
+export type FinConfig = {
+  count: number;
+  type: WindsurfFinBoxType;
+};
+
+// Types of fin box
 export enum WindsurfFinBoxType {
   TuttleBox = "TuttleBox",
   DeepTuttleBox = "DeepTuttleBox",
@@ -87,22 +144,8 @@ export enum WindsurfFinBoxType {
   PowerBox = "PowerBox",
 }
 
-export type FinConfig = {
-  count: number;
-  type: WindsurfFinBoxType;
-};
-
+// Min and max sail surfaces
 export type SailRange = {
   fromM2: number;
   toM2: number;
 };
-
-export interface WindsurfBoard<V> extends GearVariant<V> {
-  volumeL: number;
-  lengthCm?: number;
-  widthCm?: number;
-  weightKg?: number;
-  strapInsertCount?: number;
-  fins?: FinConfig[];
-  sailRange?: SailRange;
-}
