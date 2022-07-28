e7b9b3e58d65c2d50808fa0f076ccaaafac54d8e
Olivier Chirouze <o.chirouze@criteo.com>
2022-07-28 18:32:38 +0200

    0.4.13 - Introduce surf boards

--- src/model/variants/index.ts
+++ src/model/variants/index.ts
@@ -1,9 +1,13 @@
 import {WindsurfSail} from "./windsurf-sail";
 import {WindsurfBoard} from "./windsurf-board";
+import {SurfBoard} from "./surf-board";
 
 export * from "./windsurf-sail";
 export * from "./windsurf-board";
+export * from "./surf-board";
 
 export type GearSpecificVariant<VariantType> =
-  | WindsurfSail<VariantType>
-  | WindsurfBoard<VariantType>;
+    | WindsurfSail<VariantType>
+    | WindsurfBoard<VariantType>
+    | SurfBoard<VariantType>
+    ;
---
+++ src/model/variants/surf-board.ts
@@ -0,0 +1,14 @@
+// A surf board
+import {ProductVariant} from "./product-variant";
+
+export interface SurfBoard<VariantType>
+    extends ProductVariant<VariantType> {
+  // Length of the board in feet
+  lengthFt: number;
+  // Length
+  lengthCm?: number;
+  // Width
+  widthCm?: number;
+  // Width
+  thicknessCm?: number;
+}
